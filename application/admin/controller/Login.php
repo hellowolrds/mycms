01094530e9b1991affdeaf9d431362d81ab60adb
tomsj <csj504@qq.com>
2017-12-30 10:06:12 +0800

    登录功能完成

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -1,11 +1,34 @@
 <?php
 namespace app\admin\controller;
 use think\Controller;
+use think\Request;
+use think\Captcha;
+use think\Db;
 
 class Login extends Controller
 {
     public function index()
     {
         return $this->fetch('index');
+    }
+
+    public function check(Request $request)
+    {
+        if ( !$request->isPOST() ) {
+            return json(['status'=>200, 'flag'=>false, 'msg'=>'请求错误']);
+        }
+
+        $name = input('post.name');
+        $password = input('post.password');
+        $code = input('post.code');
+
+        if ( !captcha_check($code) ) {
+            return json(['status'=>200, 'flag'=>false, 'msg'=>'验证码错误']);
+        }
+        $result = Db::name('user')->where(['user_name'=>$name, 'password'=>md5($password)])->find();
+        if ( !$result ) {
+            return json(['status'=>200, 'flag'=>false, 'msg'=>'用户名或密码错误']);
+        }
+        return json(['status'=>200, 'flag'=>true, 'msg'=>'ok']);
     }
 }
